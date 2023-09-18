b92ecbf06102d013148e0e27d9e08d6d7517af45
ZUHRIDDIN-SHARIPOV <zuhriddinshx@gmail.com>
2023-09-18 19:18:44 +0500

    Updated the re-export.js file in the Pages folder.

--- src/pages/re-export.js
+++ src/pages/re-export.js
@@ -2,5 +2,5 @@ import Error from "./error/Error";
 import Home from "./home/Home";
 import Login from "./login/Login";
 import SignUp from "./sign-up/SignUp";
-import Support from "./sign-up/SignUp";
+import Support from "./support/Support";
 export { Error, Home, Login, SignUp, Support };
