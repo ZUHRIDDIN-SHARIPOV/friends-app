e2237703bb2df8aa5e6355695c4af7fcdb239dc1
ZUHRIDDIN-SHARIPOV <zuhriddinshx@gmail.com>
2023-09-18 19:02:36 +0500

    Sign Up page has been added to the site.

--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -1,9 +1,70 @@
 import { memo } from "react";
+import { IoMdFingerPrint } from "react-icons/io";
+import { Link } from "react-router-dom";
+import "./SignUp.scss";
 
 const SignUp = () => {
   return (
     <>
-      <div>SignUp</div>
+      <main>
+        <section className="signUp">
+          <div className="container">
+            <div className="signUp__block">
+              <p className="signUp__text">Welcome back 👋</p>
+              <h2 className="signUp__title">Create an account</h2>
+              <form className="signUp__form">
+                <div className="signUp__form-control">
+                  <label>
+                    Email
+                    <input
+                      type="email"
+                      name="email"
+                      autoComplete="off"
+                      placeholder="Please enter your email"
+                      required
+                    />
+                  </label>
+                  <span></span>
+                </div>
+                <div className="signUp__form-control">
+                  <label>
+                    Password
+                    <input
+                      type="password"
+                      name="password"
+                      autoComplete="off"
+                      placeholder="Enter password"
+                      required
+                    />
+                  </label>
+                  <span></span>
+                </div>
+                <div className="signUp__form-control">
+                  <label>
+                    Confirm Password
+                    <input
+                      type="password"
+                      name="password"
+                      autoComplete="off"
+                      placeholder="Confirm Password"
+                      required
+                    />
+                  </label>
+                  <span></span>
+                </div>
+                <button type="submit">
+                  Sign Up <IoMdFingerPrint />
+                </button>
+              </form>
+              <ul className="signUp__list">
+                <li className="signUp__item">
+                  <Link to={"/"}>Go to login</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
